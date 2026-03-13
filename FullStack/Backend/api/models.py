e542099f33ee5f9ca8e1fb1b9249ce18d11c3693
richochetRoch <steverochester1@gmail.com>
2026-03-13 15:01:18 +0000

    basic model work setup

--- FullStack/Backend/api/models.py
+++ FullStack/Backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    code = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class TimeEntries(models.Model):
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    date = models.DateField()
+    hours = models.FloatField()
+    
